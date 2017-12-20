ac4bb75c7c85b906d368d0150e8962f8150e6427
fenix <dima.zareckii@gmail.com>
2017-12-21 00:59:58 +0200

    First View

---
+++ YourCoin/Protols/UIProtols.swift
@@ -0,0 +1,20 @@
+//
+//  UIProtols.swift
+//  YourCoin
+//
+//  Created by Dmitriy Zaretskiy on 17.12.17.
+//  Copyright © 2017 Bubble. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol CoinButton {
+    func SetBaseCustomButton()
+    func SetSecondCustomButton()
+}
+
+protocol CoinTextField {
+    func SetCustomTextField()
+}
+
--- YourCoin/StartApp/Controllers/ViewController.swift
+++ YourCoin/StartApp/Controllers/ViewController.swift
@@ -8,16 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()  //LoadDone
+        self.performSegue(withIdentifier: "LoadDone", sender: nil)
+        
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
 
 
