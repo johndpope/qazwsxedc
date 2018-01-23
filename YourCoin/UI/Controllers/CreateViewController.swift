e57e80084b111b051b0ae00cae0757d45872ce67
Fenix <someman@somes-macbook-pro.local>
2018-01-23 21:12:37 +0200

    Temp UI create wallet

--- YourCoin/UI/Controllers/CreateViewController.swift
+++ YourCoin/UI/Controllers/CreateViewController.swift
@@ -1,32 +1,31 @@
 //
-//  AllCardsViewController.swift
+//  CreateViewController.swift
 //  YourCoin
 //
-//  Created by Some Man on 22.01.18.
+//  Created by Some Man on 23.01.18.
 //  Copyright © 2018 Bubble. All rights reserved.
 //
 
 import UIKit
 
-class AllCardsViewController: UIViewController {
+class CreateViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func CreateWalletAction(_ sender: Any) {
+        //create wallet
+        self.performSegue(withIdentifier: "GoToMain", sender: self)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    var i:Int = 1;
-    @IBAction func ыы(_ sender: Any) {
-        i+=1
-        print("Swiper", String(i))
-    }
-    
+
     /*
     // MARK: - Navigation
 
