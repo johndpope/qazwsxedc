4ec78312a674581d0126b4ca46b6b94e0fbcbfb5
Vladicheck <vladislav.dubitsky@outlook.com>
2018-01-26 08:54:22 +0200

    App Delegate

--- YourCoin/AppDelegate.swift
+++ YourCoin/AppDelegate.swift
@@ -18,13 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-//        let masterVC = WalletViewController()
-//        window = UIWindow()
-//        window?.rootViewController = masterVC
-//        window?.makeKeyAndVisible()
-        
-        
         return true
     }
 
