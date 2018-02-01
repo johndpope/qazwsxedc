1064c2e4476091ee0be2b96715b8e6d3ba7dd931
Fenix <someman@somes-macbook-pro.local>
2018-02-01 05:47:27 +0200

    Chat color

--- YourCoin/UI/Controllers/Exchange Rates/RatesViewController.swift
+++ YourCoin/UI/Controllers/Exchange Rates/RatesViewController.swift
@@ -11,33 +11,22 @@ import Foundation
 import ScrollableGraphView
 
 class RatesViewController: UIViewController, ScrollableGraphViewDataSource {
-    
- //   @IBOutlet weak var ChartView: CustomUIView!
-    @IBOutlet var graphView: ScrollableGraphView!
+
+    @IBOutlet var charthView: ScrollableGraphView!
     
     var numberOfItems = 30
     lazy var plotOneData: [Double] = self.generateRandomData(self.numberOfItems, max: 100, shouldIncludeOutliers: true)
-    lazy var plotTwoData: [Double] = self.generateRandomData(self.numberOfItems, max: 80, shouldIncludeOutliers: false)
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        graphView.dataSource = self
-        setupGraph(graphView: graphView)
+        charthView.dataSource = self
+        setupGraph(graphView: charthView)
     }
-    
-    // ScrollableGraphViewDataSource
-    // #############################
-    
+
     func value(forPlot plot: Plot, atIndex pointIndex: Int) -> Double {
-        switch(plot.identifier) {
-        case "one":
             return plotOneData[pointIndex]
-        case "two":
-            return plotTwoData[pointIndex]
-        default:
-            return 0
-        }
     }
     
     func label(atIndex pointIndex: Int) -> String {
@@ -47,45 +36,43 @@ class RatesViewController: UIViewController, ScrollableGraphViewDataSource {
     func numberOfPoints() -> Int {
         return numberOfItems
     }
-    
-    // Helper Functions
-    // ################
-    
-    // When using Interface Builder, only add the plots and reference lines in code.
+
     func setupGraph(graphView: ScrollableGraphView) {
         
         // Setup the first line plot.
-        let blueLinePlot = LinePlot(identifier: "one")
+        let chartLine = LinePlot(identifier: "one")
         
-        blueLinePlot.lineWidth = 5
-        blueLinePlot.lineColor = UIColor.colorFromHex(hexString: "#16aafc")
-        blueLinePlot.lineStyle = ScrollableGraphViewLineStyle.smooth
+        chartLine.lineWidth = 3
+        chartLine.lineColor = UIColor(red: 0.03, green: 0.88, blue: 0.57, alpha: 1)
+        chartLine.lineStyle = ScrollableGraphViewLineStyle.smooth
         
-        blueLinePlot.shouldFill = false
-        blueLinePlot.fillType = ScrollableGraphViewFillType.solid
-        blueLinePlot.fillColor = UIColor.colorFromHex(hexString: "#16aafc").withAlphaComponent(0.5)
+        chartLine.shouldFill = false
+        chartLine.fillType = ScrollableGraphViewFillType.solid
+        chartLine.fillColor = UIColor(red: 0.03, green: 0.88, blue: 0.57, alpha: 1)
         
-        blueLinePlot.adaptAnimationType = ScrollableGraphViewAnimationType.elastic
+        chartLine.adaptAnimationType = ScrollableGraphViewAnimationType.elastic
         
-        
-        // Customise the reference lines.
         let referenceLines = ReferenceLines()
-        
+
         referenceLines.referenceLineLabelFont = UIFont.boldSystemFont(ofSize: 8)
-        referenceLines.referenceLineColor = UIColor.black.withAlphaComponent(0.2)
-        referenceLines.referenceLineLabelColor = UIColor.black
-        
-        referenceLines.dataPointLabelColor = UIColor.black.withAlphaComponent(1)
-        
-        // All other graph customisation is done in Interface Builder,
-        // e.g, the background colour would be set in interface builder rather than in code.
-        // graphView.backgroundFillColor = UIColor.colorFromHex(hexString: "#333333")
-        
-        // Add everything to the graph.
+        referenceLines.referenceLineColor = UIColor.black.withAlphaComponent(0.5)
+        referenceLines.referenceLineLabelColor = UIColor.black.withAlphaComponent(0.5)
+
+        referenceLines.dataPointLabelColor = UIColor.black.withAlphaComponent(0.5)
+
         graphView.addReferenceLines(referenceLines: referenceLines)
-        graphView.addPlot(plot: blueLinePlot)
+        graphView.addPlot(plot: chartLine)
     }
     
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    
+    
+    
+    
+    //temp
     private func generateRandomData(_ numberOfItems: Int, max: Double, shouldIncludeOutliers: Bool = true) -> [Double] {
         var data = [Double]()
         for _ in 0 ..< numberOfItems {
@@ -102,7 +89,4 @@ class RatesViewController: UIViewController, ScrollableGraphViewDataSource {
         return data
     }
     
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
 }
