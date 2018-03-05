11ca3428cd52e2ef13b2a42e5e6c0ff5105da0c0
Fenix <someman@Somes-MacBook-Pro.local>
2018-03-05 20:42:26 +0200

    Refaktor

--- YourCoin/UI/Controllers/Balance/AllCards/WalletCardViewController.swift
+++ YourCoin/UI/Controllers/Balance/AllCards/WalletCardViewController.swift
@@ -25,47 +25,47 @@ class WalletCardViewController: UIViewController {
         self.view.layer.cornerRadius = 10
         self.view.layer.borderWidth = 0;
         
-        infowallelUILabel = SetLabelInfo(text:"WALLET:")
+        infowallelUILabel = SetCustomLabel(text:"WALLET:", style: "Roboto-Light")
         self.view.addSubview(infowallelUILabel)
         infowallelUILabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 22).isActive = true
         infowallelUILabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 27).isActive = true
         
-        infocurrencyUILabel = SetLabelInfo(text:"CURRENCY:")
+        infocurrencyUILabel = SetCustomLabel(text:"CURRENCY:", style: "Roboto-Light")
         self.view.addSubview(infocurrencyUILabel)
         infocurrencyUILabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 22).isActive = true
         infocurrencyUILabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 58).isActive = true
         
-        infowallelUILabel = SetLabelInfo(text:"VALUE:")
+        infowallelUILabel = SetCustomLabel(text:"VALUE:", style: "Roboto-Light")
         self.view.addSubview(infowallelUILabel)
         infowallelUILabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 22).isActive = true
         infowallelUILabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 89).isActive = true
         
         //////
         
-        wallelUILabel = SetLabelValue(text: (walletCardObject?.walletPublicKey)!)
+        wallelUILabel = SetCustomLabel(text: (walletCardObject?.walletPublicKey)!, style:"Roboto-Bold")
         self.view.addSubview(wallelUILabel)
         wallelUILabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 99).isActive = true
         wallelUILabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 27).isActive = true
         wallelUILabel.widthAnchor.constraint(equalToConstant: 209).isActive = true
         
-        vauleCurrencyUILabel = SetLabelValue(text:String(describing: (walletCardObject?.balance)!))
+        vauleCurrencyUILabel = SetCustomLabel(text:String(describing: (walletCardObject?.balance)!), style:"Roboto-Bold")
         self.view.addSubview(vauleCurrencyUILabel)
         vauleCurrencyUILabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 122).isActive = true
         vauleCurrencyUILabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 58).isActive = true
         
-        wallelUILabel = SetLabelValue(text:String((walletCardObject?.balance)!*(walletCardObject?.priceToUSD)!))
+        wallelUILabel = SetCustomLabel(text:String((walletCardObject?.balance)!*(walletCardObject?.priceToUSD)!), style:"Roboto-Bold")
         self.view.addSubview(wallelUILabel)
         wallelUILabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 84).isActive = true
         wallelUILabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 89).isActive = true
         
         //////
         
-        typeCoinCurrencyUILabel = SetLabelNameValue(text:(walletCardObject?.typeCoin)!)
+        typeCoinCurrencyUILabel = SetCustomLabel(text:(walletCardObject?.typeCoin)!, style:"Roboto-Black")
         self.view.addSubview(typeCoinCurrencyUILabel)
         typeCoinCurrencyUILabel.leftAnchor.constraint(equalTo: vauleCurrencyUILabel.rightAnchor, constant: 10).isActive = true
         typeCoinCurrencyUILabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 58).isActive = true
         
-        typeCoinUILabel = SetLabelNameValue(text:"USD")
+        typeCoinUILabel = SetCustomLabel(text:"USD", style:"Roboto-Black")
         self.view.addSubview(typeCoinUILabel)
         typeCoinUILabel.leftAnchor.constraint(equalTo: wallelUILabel.rightAnchor, constant: 10).isActive = true
         typeCoinUILabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 89).isActive = true
@@ -84,31 +84,11 @@ class WalletCardViewController: UIViewController {
 
  
 
-    private func SetLabelInfo(text:String) -> UILabel {
+    private func SetCustomLabel(text:String, style:String) -> UILabel {
         let some = UILabel()
         some.text = text
         some.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
-        some.font = UIFont(name: "Roboto-Light", size: 16)
-        some.font = some.font.withSize(16)
-        some.translatesAutoresizingMaskIntoConstraints = false
-        return some
-    }
-    
-    private func SetLabelValue(text:String) -> UILabel {
-        let some = UILabel()
-        some.text = text
-        some.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
-        some.font = UIFont(name: "Roboto-Bold", size: 16)
-        some.font = some.font.withSize(16)
-        some.translatesAutoresizingMaskIntoConstraints = false
-        return some
-    }
-    
-    private func SetLabelNameValue(text:String) -> UILabel {
-        let some = UILabel()
-        some.text = text
-        some.textColor = UIColor(red: 0.43, green: 0.92, blue: 0.89, alpha: 1)
-        some.font = UIFont(name: "Roboto-Black", size: 16)
+        some.font = UIFont(name: style, size: 16)
         some.font = some.font.withSize(16)
         some.translatesAutoresizingMaskIntoConstraints = false
         return some
